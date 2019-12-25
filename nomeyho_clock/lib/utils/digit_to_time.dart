5e3abf79341d68b07d267ccc9698940cca2d6bda
Stéphane Vanberg <vanbergstephane@gmail.com>
2019-12-25 11:46:38 +0100

    Doc

---
+++ nomeyho_clock/lib/utils/digit_to_time.dart
@@ -0,0 +1,128 @@
+import 'package:nomeyho_clock/model/time.dart';
+
+///
+/// A digit is represented by segments on a digital display
+/// (e.g. 7-segment display).
+///
+/// This method returns an array of 6 [Time] values representing the segments
+/// of the provided [digit]. A default [Time] value is returned is the digit
+/// is not in range [0-9].
+///
+/// Each time value represents up to two segments once rendered as an analog
+/// clock (one for the hour hand and one for the minute hand).
+///
+/// Combining the appropriate time values, the six clocks effectively
+/// represent the provided digit once laid out in a 2-by-3 grid.
+/// For digit 8, we have:
+/// 🕞🕤
+/// 🕒🕘
+/// 🕒🕘
+List<Time> getTimesForDigit(int digit) {
+  if (digit < 0 || digit > 9) {
+    return _defaultTimes;
+  } else {
+    return _digitToTime[digit];
+  }
+}
+
+const _defaultTimes = [
+  const Time(hours: 7.5, minutes: 37.5),
+  const Time(hours: 7.5, minutes: 37.5),
+  const Time(hours: 7.5, minutes: 37.5),
+  const Time(hours: 7.5, minutes: 37.5),
+  const Time(hours: 7.5, minutes: 37.5),
+  const Time(hours: 7.5, minutes: 37.5)
+];
+
+const _digitToTime = [
+  // 0
+  [
+    const Time(hours: 6, minutes: 15),
+    const Time(hours: 9, minutes: 30),
+    const Time(hours: 6, minutes: 0),
+    const Time(hours: 0, minutes: 30),
+    const Time(hours: 3, minutes: 0),
+    const Time(hours: 0, minutes: 45),
+  ],
+  // 1
+  [
+    const Time(hours: 7.5, minutes: 37.5),
+    const Time(hours: 6, minutes: 30),
+    const Time(hours: 7.5, minutes: 37.5),
+    const Time(hours: 6, minutes: 0),
+    const Time(hours: 7.5, minutes: 37.5),
+    const Time(hours: 0, minutes: 0)
+  ],
+  // 2
+  [
+    const Time(hours: 3, minutes: 15),
+    const Time(hours: 9, minutes: 30),
+    const Time(hours: 6, minutes: 15),
+    const Time(hours: 0, minutes: 45),
+    const Time(hours: 0, minutes: 15),
+    const Time(hours: 9, minutes: 45)
+  ],
+  // 3
+  [
+    const Time(hours: 3, minutes: 15),
+    const Time(hours: 9, minutes: 30),
+    const Time(hours: 3, minutes: 15),
+    const Time(hours: 9, minutes: 0),
+    const Time(hours: 3, minutes: 15),
+    const Time(hours: 9, minutes: 0)
+  ],
+  // 4
+  [
+    const Time(hours: 6, minutes: 30),
+    const Time(hours: 6, minutes: 30),
+    const Time(hours: 0, minutes: 15),
+    const Time(hours: 6, minutes: 0),
+    const Time(hours: 7.5, minutes: 37.5),
+    const Time(hours: 0, minutes: 0)
+  ],
+  // 5
+  [
+    const Time(hours: 6, minutes: 15),
+    const Time(hours: 9, minutes: 45),
+    const Time(hours: 0, minutes: 15),
+    const Time(hours: 6, minutes: 45),
+    const Time(hours: 3, minutes: 15),
+    const Time(hours: 0, minutes: 45)
+  ],
+  // 6
+  [
+    const Time(hours: 6, minutes: 15),
+    const Time(hours: 9, minutes: 45),
+    const Time(hours: 6, minutes: 0),
+    const Time(hours: 6, minutes: 45),
+    const Time(hours: 0, minutes: 15),
+    const Time(hours: 0, minutes: 45)
+  ],
+  // 7
+  [
+    const Time(hours: 3, minutes: 15),
+    const Time(hours: 6, minutes: 45),
+    const Time(hours: 7.5, minutes: 37.5),
+    const Time(hours: 6, minutes: 0),
+    const Time(hours: 7.5, minutes: 37.5),
+    const Time(hours: 0, minutes: 0)
+  ],
+  // 8
+  [
+    const Time(hours: 6, minutes: 15),
+    const Time(hours: 6, minutes: 45),
+    const Time(hours: 0, minutes: 15),
+    const Time(hours: 0, minutes: 45),
+    const Time(hours: 0, minutes: 15),
+    const Time(hours: 0, minutes: 45)
+  ],
+  // 9
+  [
+    const Time(hours: 6, minutes: 15),
+    const Time(hours: 6, minutes: 45),
+    const Time(hours: 3, minutes: 0),
+    const Time(hours: 6, minutes: 0),
+    const Time(hours: 3, minutes: 15),
+    const Time(hours: 0, minutes: 45)
+  ],
+];
--- nomeyho_clock/lib/widget/digit.dart
+++ nomeyho_clock/lib/widget/digit.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nomeyho_clock/model/time.dart';
+import 'package:nomeyho_clock/utils/digit_to_time.dart';
 import 'package:nomeyho_clock/utils/zip.dart';
 import 'package:nomeyho_clock/widget/clock.dart';
 
@@ -11,8 +11,8 @@ class Digit extends StatelessWidget {
   }) : assert(digit >= 0 && digit <= 9);
 
   _buildClocks() {
-    final previousTimes = digit > 0 ? _digitToTime[digit - 1] : _defaultTimes;
-    final times = _digitToTime[digit];
+    final previousTimes = getTimesForDigit(digit - 1);
+    final times = getTimesForDigit(digit);
 
     return zip([previousTimes, times])
         .map((t) => Clock(previousTime: t[0], time: t[1]))
@@ -31,105 +31,3 @@ class Digit extends StatelessWidget {
     );
   }
 }
-
-const _defaultTimes = [
-  Time(hours: 7.5, minutes: 37.5),
-  Time(hours: 7.5, minutes: 37.5),
-  Time(hours: 7.5, minutes: 37.5),
-  Time(hours: 7.5, minutes: 37.5),
-  Time(hours: 7.5, minutes: 37.5),
-  Time(hours: 7.5, minutes: 37.5)
-];
-
-const _digitToTime = [
-  // 0
-  [
-    Time(hours: 6, minutes: 15),
-    Time(hours: 9, minutes: 30),
-    Time(hours: 6, minutes: 0),
-    Time(hours: 0, minutes: 30),
-    Time(hours: 3, minutes: 0),
-    Time(hours: 0, minutes: 45),
-  ],
-  // 1
-  [
-    Time(hours: 7.5, minutes: 37.5),
-    Time(hours: 6, minutes: 30),
-    Time(hours: 7.5, minutes: 37.5),
-    Time(hours: 6, minutes: 0),
-    Time(hours: 7.5, minutes: 37.5),
-    Time(hours: 0, minutes: 0)
-  ],
-  // 2
-  [
-    Time(hours: 3, minutes: 15),
-    Time(hours: 9, minutes: 30),
-    Time(hours: 6, minutes: 15),
-    Time(hours: 0, minutes: 45),
-    Time(hours: 0, minutes: 15),
-    Time(hours: 9, minutes: 45)
-  ],
-  // 3
-  [
-    Time(hours: 3, minutes: 15),
-    Time(hours: 9, minutes: 30),
-    Time(hours: 3, minutes: 15),
-    Time(hours: 9, minutes: 0),
-    Time(hours: 3, minutes: 15),
-    Time(hours: 9, minutes: 0)
-  ],
-  // 4
-  [
-    Time(hours: 6, minutes: 30),
-    Time(hours: 6, minutes: 30),
-    Time(hours: 0, minutes: 15),
-    Time(hours: 6, minutes: 0),
-    Time(hours: 7.5, minutes: 37.5),
-    Time(hours: 0, minutes: 0)
-  ],
-  // 5
-  [
-    Time(hours: 6, minutes: 15),
-    Time(hours: 9, minutes: 45),
-    Time(hours: 0, minutes: 15),
-    Time(hours: 6, minutes: 45),
-    Time(hours: 3, minutes: 15),
-    Time(hours: 0, minutes: 45)
-  ],
-  // 6
-  [
-    Time(hours: 6, minutes: 15),
-    Time(hours: 9, minutes: 45),
-    Time(hours: 6, minutes: 0),
-    Time(hours: 6, minutes: 45),
-    Time(hours: 0, minutes: 15),
-    Time(hours: 0, minutes: 45)
-  ],
-  // 7
-  [
-    Time(hours: 3, minutes: 15),
-    Time(hours: 6, minutes: 45),
-    Time(hours: 7.5, minutes: 37.5),
-    Time(hours: 6, minutes: 0),
-    Time(hours: 7.5, minutes: 37.5),
-    Time(hours: 0, minutes: 0)
-  ],
-  // 8
-  [
-    Time(hours: 6, minutes: 15),
-    Time(hours: 6, minutes: 45),
-    Time(hours: 0, minutes: 15),
-    Time(hours: 0, minutes: 45),
-    Time(hours: 0, minutes: 15),
-    Time(hours: 0, minutes: 45)
-  ],
-  // 9
-  [
-    Time(hours: 6, minutes: 15),
-    Time(hours: 6, minutes: 45),
-    Time(hours: 3, minutes: 0),
-    Time(hours: 6, minutes: 0),
-    Time(hours: 3, minutes: 15),
-    Time(hours: 0, minutes: 45)
-  ],
-];
